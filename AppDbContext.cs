ccec15fb6db7d3b00e59c2d05e0804611fc38ef4
baromain <romain.barreau31@ynov.com>
2026-05-19 11:42:59 +0200

    feat(workspaces): add workspace entity, controller and migration

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -5,6 +5,7 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
+    public DbSet<Workspace> Workspaces { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
