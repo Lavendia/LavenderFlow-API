c9da48f3525a95bdb9af4447693060191dbbb5de
Zeteox <zeteox01@gmail.com>
2026-05-19 14:39:42 +0200

    feat: added the list items table

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -7,12 +7,25 @@ public class AppDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Workspace> Workspaces { get; set; }
     public DbSet<Board> Boards { get; set; }
+    public DbSet<ListItem> ListItems { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>()
             .HasIndex(u => u.Email)
             .IsUnique();
+
+        modelBuilder.Entity<Board>()
+            .HasOne(b => b.Workspace)
+            .WithMany()
+            .HasForeignKey(b => b.WorkspaceId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<ListItem>()
+            .HasOne(l => l.Board)
+            .WithMany()
+            .HasForeignKey(l => l.BoardId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 
     public override int SaveChanges()
