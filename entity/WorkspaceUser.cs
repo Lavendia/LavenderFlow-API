cdb50dae92846e9e08c8990a3f331f79adf2a818
Zeteox <zeteox01@gmail.com>
2026-05-22 11:34:21 +0200

    fix: workspace user wasnt extending base entity

--- entity/WorkspaceUser.cs
+++ entity/WorkspaceUser.cs
@@ -1,16 +1,16 @@
 using System.Diagnostics.CodeAnalysis;
 
-public class WorkspaceUser
+public class WorkspaceUser : BaseEntity
 {
     public int Id { get; set; }
-    public int UserId { get; set; }
-    public User? User { get; set; }
-    public int WorkspaceId { get; set; }
-    public Workspace? Workspace { get; set; }
-    public int RoleId { get; set; }
-    public WorkspaceRole? Role { get; set; }
+    public required int UserId { get; set; }
+    public required User User { get; set; }
 
-    public WorkspaceUser() {}
+    public required int WorkspaceId { get; set; }
+    public required Workspace Workspace { get; set; }
+
+    public required int RoleId { get; set; }
+    public required WorkspaceRole Role { get; set; }
 
     [SetsRequiredMembers]
     public WorkspaceUser(int userId, int workspaceId, int roleId)
@@ -18,7 +18,5 @@ public class WorkspaceUser
         UserId = userId;
         WorkspaceId = workspaceId;
         RoleId = roleId;
-        CreatedAt = DateTime.UtcNow;
-        UpdatedAt = DateTime.UtcNow;
     }
 }
